e4b155dc6aecc402ad83ccf3fbd0172b4c3d1bc9
bala-amypo <bala@amypo.in>
2025-12-17 06:04:44 +0000

    User commit

--- src/main/java/com/example/demo/controller/Studentcontroller.java
+++ src/main/java/com/example/demo/controller/Studentcontroller.java
@@ -2,4 +2,24 @@ package com.example.demo.controller;
 import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import com.example
+import com.example.demo.entity.Studententity;
+import com.example.demo.service.Studentservice;
+@RestController
+@RequestMapping("/student")
+public class Studentcontroller{
+    @Autowired
+    private Studentservice ser;
+    @Post mapping("/add")
+    public Studententity addStudent(@RequestBody Studententity st){
+        return ser.saveData(st);
+    }
+    @GetMapping("/getall")
+    public Collection<Studententity>getAllStudents(){
+        return ser.getAll();
+        @GetMapping("/get/{id}")
+        public
+    }
+
+    }
+    }
+}
